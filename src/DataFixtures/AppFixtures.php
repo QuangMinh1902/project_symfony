815b6749a49f15fa550ca8e1031b56325a89e1e1
Quang Minh Pham <quangminh1575@gmail.com>
2023-02-06 16:26:29 +0100

    create data table

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Article;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,16 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 0; $i < 22; $i++) {
+            $article = new Article();
+            $article->setName("Nike"  . "75$i");
+            $article->setPrix(rand(100, 400));
+            $article->setDescription("This is a Nike pair with the number 75$i");
+            $article->setNombresAvailable($i + rand(1, 30));
+            $article->setCreateAt(new \DateTimeImmutable());
+
+            $manager->persist($article);
+        }
 
         $manager->flush();
     }
