a28942b0bd302d46e6b5295debe3fac4e13a42d9
Quang Minh Pham <quangminh1575@gmail.com>
2023-02-07 21:35:33 +0100

    updated table + paginator

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -8,21 +8,34 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 use App\Entity\Article;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Doctrine\Persistence\ManagerRegistry;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ArticleController extends AbstractController
 {
+    /**
+     * show all articles
+     *
+     * @param ManagerRegistry $doctrine
+     * @param PaginatorInterface $paginator
+     * @param Request $request
+     * @return Response
+     */
     #[Route('/articles', name: 'app_article')]
-    public function showAricles(ManagerRegistry $doctrine): Response
+    public function showAricles(ManagerRegistry $doctrine, PaginatorInterface $paginator, Request $request): Response
     {
         $repository = $doctrine->getRepository(Article::class);
-        $articles = $repository->findAll();
-
+        $articles = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt("page", 1),
+            10
+        );
         return $this->render('article/show_all_articles.html.twig', [
-            'articles' => $articles,
+            'articles' => $articles
         ]);
     }
 
