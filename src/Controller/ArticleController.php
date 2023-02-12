85833a72a398d761586cd48daa28e0c7d345b050
Quang Minh Pham <qpham1@myges.fr>
2023-02-12 22:08:39 +0100

    add create function

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -8,6 +8,9 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 use App\Entity\Article;
+use App\Form\ArticleType;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,7 +18,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ArticleController extends AbstractController
+
+class ArticleController extends AbstractController  
 {
     /**
      * show all articles
@@ -47,4 +51,26 @@ class ArticleController extends AbstractController
         $entityManager->flush();
         return $this->redirectToRoute('app_article');
     }
+
+    #[Route("/article/create", name: "create_article", methods: ["GET", "POST"])]
+    public function createArticle(Request $request,EntityManagerInterface $manager): Response
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request); 
+        if($form->isSubmitted() && $form->isValid() ){
+            $article= $form->getData();
+            $manager->persist($article);
+            $manager->flush();
+            $this->addFlash(
+                'success',
+                'New article created successfully'
+            );
+        }
+        return $this->render("article/new_article.html.twig", [
+            'form' => $form->createView()
+        ]);
+    }
+
+
 }
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\ArticleRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ArticleRepository::class)]
 class Article
@@ -15,18 +16,32 @@ class Article
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    #[Assert\Length(
+        min: 2,
+        max: 20,
+        minMessage: 'Your first name must be at least {{ limit }} characters long',
+        maxMessage: 'Your first name cannot be longer than {{ limit }} characters',
+    )]
     private ?string $name = null;
 
     #[ORM\Column(type: Types::TEXT)]
     private ?string $description = null;
 
     #[ORM\Column]
+    #[Assert\NotBlank]
+    #[Assert\Positive]
+    #[Assert\GreaterThan(5)]
+    #[Assert\LessThan(200)]
     private ?int $prix = null;
 
-    #[ORM\Column]
+    #[ORM\Column(nullable: true)]
     private ?\DateTimeImmutable $createAt = null;
 
     #[ORM\Column]
+    #[Assert\NotBlank]
+    #[Assert\Positive]
+    #[Assert\LessThan(500)]
     private ?int $nombresAvailable = null;
 
 
@@ -78,8 +93,10 @@ class Article
 
     public function setCreateAt(\DateTimeImmutable $createAt): self
     {
+        // if ($this->getCreateAt() === null) {
+        //     $this->setCreateAt(new \DateTimeImmutable('now'));
+        // }
         $this->createAt = $createAt;
-
         return $this;
     }
 
@@ -94,5 +111,4 @@ class Article
 
         return $this;
     }
-
 }
